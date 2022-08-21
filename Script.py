ca6e93e4b00d179025c20c96f1cbfb1adcc949e8
manohartech52 <84001624+manohartech52@users.noreply.github.com>
2022-08-21 18:52:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TXT = """HOW TO USE <a href=https://t.me/TBORIGINALS/52{}>{}</a>, WATCH BEFORE USING ME"""
+    START_TXT = """HOW TO USE <a href=https://t.me/TBORIGINALS/52</a>, WATCH BEFORE USING ME"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
