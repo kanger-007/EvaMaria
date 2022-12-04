c0a9adfedb1178a09d3add207a6da16bab02785e
Nunnagoppala Teja <67798221+kangScripter@users.noreply.github.com>
2022-12-04 18:24:35 +0530

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
 
-UPSTREAM_REPO = "https://github.com/manohartech52/EvaMaria"#environ.get('UPSTREAM_REPO')
+UPSTREAM_REPO = "https://github.com/Anubisop17/Tragic"#environ.get('UPSTREAM_REPO')
 UPSTREAM_BRANCH = "master" 
 try:
     if len(UPSTREAM_REPO) == 0:
